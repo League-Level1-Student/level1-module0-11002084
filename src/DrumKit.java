0f97468c9fdd2802d0fda3943d82c5fcff76c031
AnkitBisain <league@WTS10.attlocal.net>
2018-09-30 15:57:56 -0700

    Drum Kit, Demanding Buttons

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -20,7 +21,7 @@ import javax.swing.JPanel;
 public class DrumKit implements MouseListener {
 
 	JLabel drumLabelWithImage;
-
+	JLabel cymbalLabelWithImage;
 	public static void main(String[] args) throws Exception {
 		new DrumKit().getGoing();
 	}
@@ -31,31 +32,35 @@ public class DrumKit implements MouseListener {
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Set the size of the frame
 		frame.setSize(500, 500);
 		// 4. Set the title of the frame
 		frame.setTitle("Drumkit");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-		JPanel panel=new JPanel();
+		JPanel panel = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
 		frame.add(panel);
 		// 7. Download an image of a drum from the Internet. Drop it into your Eclipse
 		// project under "default package".
 		// 8. Put the name of your image in a String variable.
-String drum="THIS IS A DRUM.jpg";
+		String drum = "SNARE DRUM.jpg";
+		String cymbal = "CYMBAL.png";
 		// 9. Edit the next line to use your String variable
 		drumLabelWithImage = createLabelImage(drum);
+		cymbalLabelWithImage = createLabelImage(cymbal);
 
 		// 10. Add the image to the panel
-panel.add(drumLabelWithImage);
+		panel.add(drumLabelWithImage);
+		panel.add(cymbalLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-panel.setLayout(new GridLayout());
+				panel.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see your
 		// drum image?
-frame.pack();
+		frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-drumLabelWithImage.addMouseListener(this);
+		drumLabelWithImage.addMouseListener(this);
+		cymbalLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse listener to
 		// each one.
 
@@ -64,13 +69,20 @@ drumLabelWithImage.addMouseListener(this);
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch the
 		// console to see when this is printed.
-
-		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label that the mouse clicked on
+		System.out.println("mouse clicked");
+		JLabel drumClicked = (JLabel) e.getSource();// This line gets the label that the mouse clicked on
 
 		// 15. Download a drum sound and drop it into your "default package". You can
 		// find it on freesound.org. To download it, log in as
 		// leagueofamazing/code4life.
 		// 16. If they clicked on the drumImage...
+		if (drumLabelWithImage.equals(drumClicked)) {
+			playSound("snare.wav");
+		}
+		if (cymbalLabelWithImage.equals(drumClicked)) {
+			playSound("cymbal.wav");
+		}
+		
 
 		// 17. ...use the playSound method to play a drum sound. Test to see if it works
 
--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -27,26 +27,27 @@ public class DuelingButtons implements ActionListener {
 	JPanel panel = new JPanel();
 
 	private void createUI() {
+		JFrame frame =new JFrame();
 		// 1. Add the panel to the frame
-
+frame.add(panel);
 		// 2. Make the frame visible
-
+frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+leftButton.setText("CLICK ME NOW OR ELSE");
 		// 4. Set the text of the rightButton to "Click me!"
-
+rightButton.setText("CLICK ME IF YOU CLICK HIM YOU WILL DIE");
 		// 5. Add an action listener to the leftButton
-
+leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+panel.add(rightButton);
 		// 9. Pack the frame
-
+frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
